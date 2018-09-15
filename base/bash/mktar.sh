1d903d8dfe82aee32daa466cf898306dac63cdf8
Ivailo Monev <xakepa10@laimg.moc>
2018-09-15 21:18:21 +0000

    iproute2 update and some fixes
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- base/bash/mktar.sh
+++ base/bash/mktar.sh
@@ -8,6 +8,6 @@ if [ -z "$1" ];then
 fi
 git clone --depth="$1" git://git.sv.gnu.org/bash.git
 cd bash
-git archive --prefix="bash-4.3.$1/" HEAD | xz > "../bash-4.3.$1.tar.xz"
+git archive --prefix="bash-4.4.$1/" HEAD | xz > "../bash-4.4.$1.tar.xz"
 cd -
 rm -rf bash
