3254ca20fcc7d0579a23e90abb662b56fae1140b
Ivailo Monev <xakepa10@gmail.com>
2020-12-25 17:18:33 +0000

    preperations for use with pkgsrc
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- base/filesystem/extrapaths.sh
+++ base/filesystem/extrapaths.sh
@@ -11,3 +11,16 @@ fi
 # Set some defaults before other applications add to these paths.
 pathappend /usr/share/man  MANPATH
 pathappend /usr/share/info INFOPATH 
+
+# pkgsrc paths.
+if [ -d /usr/pkg/bin ]; then
+        pathappend /usr/pkg/bin
+fi
+
+if [ -d /usr/pkg/sbin -a $EUID -eq 0 ]; then
+        pathappend /usr/pkg/sbin
+fi
+
+if [ -d /usr/pkg/man ]; then
+        pathappend /usr/pkg/man MANPATH
+fi
--- extra/ccache/profile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ "$EUID" -ne "0" ];then
-    export PATH="/usr/lib/ccache/bin/:$PATH"
-fi
